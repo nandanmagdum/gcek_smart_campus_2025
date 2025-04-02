98fe81c6d5f901fa17665ff8c2853e74fa59ae3b
nandanmagdum <nandanmagdum14@gmail.com>
2025-04-02 15:55:23 +0530

    configured flutter and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_campus/constants/app_colors.dart';
 import 'package:smart_campus/controllers/theme_controller.dart';
+import 'package:smart_campus/firebase_options.dart';
 import 'package:smart_campus/pages/home_page.dart';
 import 'package:get/get.dart';
 import 'package:smart_campus/utils/local_storage.dart';
@@ -8,6 +10,7 @@ import 'package:smart_campus/utils/navigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await LocalStorage.init();
   Get.lazyPut(() => ThemeController());
   runApp(MyApp());
