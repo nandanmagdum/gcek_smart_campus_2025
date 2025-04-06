83f6ccd8d45839458150af1e175eb41ef3f1745d
nandanmagdum <nandanmagdum14@gmail.com>
2025-04-06 18:22:42 +0530

    animation try 1

--- lib/widgets/title_container.dart
+++ lib/widgets/title_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:smart_campus/constants/app_colors.dart';
 
 class TitleContainer extends StatelessWidget {
@@ -9,20 +10,26 @@ class TitleContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      decoration: BoxDecoration(
-        color: AppColors.palate2,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      width: MediaQuery.of(context).size.width * 0.9,
-      child: Center(
-        child: Text(
-          title,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+    return Animate(
+      effects: [
+        FadeEffect(),
+        SlideEffect(),
+      ],
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          color: AppColors.palate2,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        width: MediaQuery.of(context).size.width * 0.9,
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
